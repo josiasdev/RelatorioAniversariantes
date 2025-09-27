a299f2cf13dd7f249b5bccbe1055f6d5369943e4
josiasdev <francisco.batista67@alu.ufc.br>
2025-09-27 19:35:45 -0300

    feat: Adicionando atributos para pegar os valores do application properties

--- src/main/java/com/github/josiasdev/RelatorioAniversariantes/service/RelatorioAniversariantesService.java
+++ src/main/java/com/github/josiasdev/RelatorioAniversariantes/service/RelatorioAniversariantesService.java
@@ -1,4 +1,45 @@
 package com.github.josiasdev.RelatorioAniversariantes.service;
 
+import com.github.josiasdev.RelatorioAniversariantes.dto.AniversarianteDTO;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Service
 public class RelatorioAniversariantesService {
+
+    @Value("${app.credentials.client-code}")
+    private String clientCode;
+
+    @Value("${app.credentials.username}")
+    private String username;
+
+    @Value("${app.credentials.password}")
+    private String password;
+
+
 }
