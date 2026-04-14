620514cd1af228a7d45a31bd2506cb575d74284e
josiasdev <josiasmartins098@gmail.com>
2026-04-13 21:36:19 -0300

    docs(config): atualiza metadados do OpenAPI com informações de licença e servidor

--- src/main/java/com/github/josiasdev/RelatorioAniversariantes/config/OpenApiConfig.java
+++ src/main/java/com/github/josiasdev/RelatorioAniversariantes/config/OpenApiConfig.java
@@ -4,23 +4,30 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class OpenApiConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("http://localhost:8080").description("Servidor Local")))
                 .info(new Info()
-                        .title("API de Geração de Relatórios")
-                        .description("API responsável pela automação da geração de relatórios de aniversariantes da igreja.")
+                        .title("Church Report Automation API")
+                        .description("Sistema inteligente para extração, processamento e geração de relatórios " +
+                                "automatizados a partir da plataforma Church Software.")
                         .version("v1.0.0")
                         .contact(new Contact()
                                 .name("Josias Batista")
                                 .email("josiasmartins098@gmail.com")
                                 .url("https://www.linkedin.com/in/josias-batista/"))
-                );
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
     }
-
 }
